170e48b52157045d6975d76ba76434914e94ae97
yoonday <yoon2dev@gmail.com>
2024-10-21 13:06:58 +0900

    solve: Solve requirement 1 of Q1
    - 스탠다드 2주차 Q1. [구현사항 1] 해결

--- Assets/Scripts/Manager/QuestManager.cs
+++ Assets/Scripts/Manager/QuestManager.cs
@@ -1,18 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 
 public class QuestManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // Q1 [구현사항 1]
+    private static QuestManager instance;
 }
