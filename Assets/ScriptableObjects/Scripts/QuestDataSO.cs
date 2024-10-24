e8ee4a82c8ae1319416a7fea82ae7575c025a50a
yoonday <yoon2dev@gmail.com>
2024-10-24 12:17:47 +0900

    solve: Solve requirement 1 of Q3
    - 스탠다드 2주차 Q3. [구현사항 1] 해결

---
+++ Assets/ScriptableObjects/Scripts/QuestDataSO.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Q3 [구현사항 1]
+[CreateAssetMenu(fileName = "DefaultQuestSO", menuName = "QuestData", order = 0)]
+public class QuestDataSO : ScriptableObject
+{
+    [Header("Quest Info")]
+    public string QuestName;
+    public int QuestRequiredLevel;
+    public int QuestNPC;
+    public List<int> QestPrerequisites;
+}
